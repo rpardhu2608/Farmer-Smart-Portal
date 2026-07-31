53b9494850c704811457835dd1be33eb57743635
rpardhu2608 <rpardhu2608@gmail.com>
2026-07-31 21:55:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 # OpenWeatherMap API Key
-WEATHER_API_KEY = "cb0bd1fb49b211d8d604b4371fb646ae"
+WEATHER_API_KEY = ""
 
 # Plant.id API Key (for future crop disease detection)
-PLANT_API_KEY = "Jpa6CbbM7DwbmPBQlyVFPrUneXKTPNmFRbU4aezIlzI8Ty9IHh"
+PLANT_API_KEY = ""
 
 # Google Maps API Key (optional)
 GOOGLE_MAPS_API_KEY = "YOUR_GOOGLE_MAPS_API_KEY"
